40763768a86a1bfe6a1b0d7f57d5f674df406303
Ilya Belan <iliyars@mail.ru>
2026-01-07 03:28:47 +0300

    DbSet<FormTypeDto> -> DbSet<FormType>

--- ElectronicMaps.Infrastructure/Persistence/Repositories/Queries/Forms/EfFormTypeReadRepository.cs
+++ ElectronicMaps.Infrastructure/Persistence/Repositories/Queries/Forms/EfFormTypeReadRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ElectronicMaps.Application.Abstractions.Queries.Forms;
 using ElectronicMaps.Application.DTOs.Forms;
+using ElectronicMaps.Domain.Entities;
 using ElectronicMaps.Infrastructure.Persistence.Configuration;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,26 +17,26 @@ namespace ElectronicMaps.Infrastructure.Persistence.Repositories.Queries.Forms
 
         public Task<IReadOnlyList<FormTypeDto>> GetAllAsync(CancellationToken ct)
         {
-            return _db.Set<FormTypeDto>().AsNoTracking()
+            return _db.Set<FormType>().AsNoTracking()
                 .OrderBy(x => x.Code)
-                .Select(x => new FormTypeDto(x.Id, x.Code, x.Name))
+                .Select(x => new FormTypeDto(x.Id, x.Code, x.DisplayName))
                 .ToListAsync(ct)
                 .ContinueWith(t => (IReadOnlyList<FormTypeDto>)t.Result, ct);
         }
 
         public Task<FormTypeDto?> GetByCodeAsync(string code, CancellationToken ct)
         {
-            return _db.Set<FormTypeDto>().AsNoTracking()
+            return _db.Set<FormType>().AsNoTracking()
                 .Where(x => x.Code == code)
-                .Select(x => new FormTypeDto(x.Id, x.Code, x.Name))
+                .Select(x => new FormTypeDto(x.Id, x.Code, x.DisplayName))
                 .FirstOrDefaultAsync(ct);
         }
 
         public Task<FormTypeDto?> GetByIdAsync(int id, CancellationToken ct)
         {
-            return _db.Set<FormTypeDto>().AsNoTracking()
+            return _db.Set<FormType>().AsNoTracking()
                 .Where(x => x.Id == id)
-                .Select(x => new FormTypeDto(x.Id, x.Code, x.Name))
+                .Select(x => new FormTypeDto(x.Id, x.Code, x.DisplayName))
                 .FirstOrDefaultAsync(ct);
         }
     }
