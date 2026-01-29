d13b2ad9a35ad7c3f37a350297cfa9294025683e
Ilya <iliyars@gmail.com>
2026-01-29 10:39:54 +0300

    исправлен возвращаемый DialogResult при закрытии модального окна

--- ElectronicMaps.WPF/Features/Workspace/Components/CreateComponentDialog.xaml.cs
+++ ElectronicMaps.WPF/Features/Workspace/Components/CreateComponentDialog.xaml.cs
@@ -14,32 +14,35 @@ using System.Windows.Shapes;
 
 namespace ElectronicMaps.WPF.Features.Workspace.Components
 {
-    /// <summary>
-    /// Interaction logic for CreateComponentDialog.xaml
-    /// </summary>
-    public partial class CreateComponentDialog : Window
+  /// <summary>
+  /// Interaction logic for CreateComponentDialog.xaml
+  /// </summary>
+  public partial class CreateComponentDialog : Window
+  {
+
+    private readonly CreateComponentViewModel _viewModel;
+    public CreateComponentDialog(CreateComponentViewModel viewModel)
     {
+      InitializeComponent();
 
-        private readonly CreateComponentViewModel _viewModel;
-        public CreateComponentDialog(CreateComponentViewModel viewModel)
-        {
-            InitializeComponent();
-
-            _viewModel = viewModel;
-            DataContext = _viewModel;
+      _viewModel = viewModel;
+      DataContext = _viewModel;
 
 
-            _viewModel.CloseAction = () => Close();
-        }
-
-        public bool? ShowDialogWithDraft(ComponentDraft draft)
-        {
-            Loaded += async (s, e) =>
-            {
-                await _viewModel.InitializeAsync(draft);
-            };
-
-            return ShowDialog();
-        }
+      _viewModel.CloseAction = () =>
+      {
+        this.DialogResult = _viewModel.DialogResult;
+      };
     }
+
+    public bool? ShowDialogWithDraft(ComponentDraft draft)
+    {
+      Loaded += async (s, e) =>
+      {
+        await _viewModel.InitializeAsync(draft);
+      };
+
+      return ShowDialog();
+    }
+  }
 }
